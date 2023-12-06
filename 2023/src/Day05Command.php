5d8f3f1ddce2910b335fc6ebc2aa609d1d2e3084
Enrico Beck <enrico.beck@mellowmessage.de>
2023-12-06 20:48:25 +0100

    day 5 part 1

--- 2023/src/Day05Command.php
+++ 2023/src/Day05Command.php
@@ -6,19 +6,39 @@ use Symfony\Component\Console\Attribute\AsCommand;
 
 #[AsCommand(
     name: 'day05',
-    description: 'Day 05: ???'
+    description: 'Day 05: If You Give A Seed A Fertilizer'
 )]
 class Day05Command extends AbstractCommand
 {
 
     protected function part1($input): int
     {
-        $values = [];
-        $lines = explode("\n", $input);
-        foreach ($lines as $line) {
-            $values[] = 0;
+        list($seedLine, $seedToSoilMap, $soilToFertilizerMap, $fertilizerToWaterMap, $waterToLightMap,
+            $lightToTemperatureMap, $temperatureToHumidityMap, $humidityToLocationMap) = explode("\n\n", $input);
+
+        $conversions = [
+            'seed' => 'soil',
+            'soil' => 'fertilizer',
+            'fertilizer' => 'water',
+            'water' => 'light',
+            'light' => 'temperature',
+            'temperature' => 'humidity',
+            'humidity' => 'location',
+        ];
+
+        $locations = [];
+        $seeds = explode(' ', str_replace('seeds: ', '', $seedLine));
+        foreach ($seeds as $seed) {
+            foreach ($conversions as $from => $to) {
+                $map = ${$from . 'To' . ucfirst($to) . 'Map'};
+                $destination = $this->getDestination($map, $from === 'seed' ? $seed : $destination);
+                if ($to === 'location') {
+                    $locations[] = $destination;
+                }
+            }
         }
-        return array_sum($values);
+
+        return min($locations);
     }
 
     protected function part2($input): int
@@ -30,5 +50,18 @@ class Day05Command extends AbstractCommand
         }
         return array_sum($values);
     }
-    
+
+    private function getDestination(string $map, int $source): int
+    {
+        $lines = explode("\n", $map);
+        for ($i = 1; $i < count($lines); $i++) {
+            $line = $lines[$i];
+            list($destinationStart, $sourceStart, $range) = array_map('intval', explode(' ', $line));
+            if ($source >= $sourceStart && $source < $sourceStart + $range - 1) {
+                return $destinationStart + $source - $sourceStart;
+            }
+        }
+        return $source;
+    }
+
 }
